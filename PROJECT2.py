cbceaeea4c77f41fc3f10c3a094299217fe798b6
RAJDEEP21SINGH <158617221+RAJDEEP21SINGH@users.noreply.github.com>
2024-02-25 12:04:51 +0530

    Add files via upload

---
+++ PROJECT2.py
@@ -0,0 +1,7 @@
+#it is a general extension of the message sender.
+#it can be used in any project.
+#as after sending the message it must shutdown the system automatically.
+
+import os 
+
+os.system("shutdown /s  /t 15") #it will shutdown the system  automatically after 15sec
